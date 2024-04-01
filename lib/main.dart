137ae3ffa78fbb2622bcdab6fa81a1645c4e6f4b
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-04-01 14:55:35 +0900

    feat: GetX 및 flutter_screenutil 초기화
    - 임시 login view 추가
    
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gabozago/app/config/app_binding.dart';
+import 'package:gabozago/app/config/routes/route_path.dart';
+import 'package:gabozago/app/config/routes/routes.dart';
+import 'package:get/route_manager.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +14,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return ScreenUtilInit(
+      designSize: const Size(393, 852),
+      minTextAdapt: true,
+      useInheritedMediaQuery: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          builder: (context, child) {
+            return MediaQuery(data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.0)), child: child!);
+          },
+          initialBinding: AppBinding(),
+          initialRoute: RoutePath.login,
+          getPages: Routes.getPages,
+        );
+      },
     );
   }
 }
