c8e164cafbe828ea0907983681a2119c72efa2ab
HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>
2024-04-01 14:55:35 +0900

    feat: GetX 및 flutter_screenutil 초기화
    - 임시 login view 추가
    
    Signed-off-by: HeeRyun, Jin(qwertycvb) <jins4218@gmail.com>

--- lib/app/config/routes/routes.dart
+++ lib/app/config/routes/routes.dart
@@ -1,5 +1,13 @@
+import 'package:gabozago/presentation/login/view/login_view.dart';
+import 'package:gabozago/presentation/login/view_model/login_binding.dart';
 import 'package:get/route_manager.dart';
 
 class Routes {
-  static List<GetPage> getPages = [];
+  static List<GetPage> getPages = [
+    GetPage(
+      name: '/login',
+      page: () => const LoginView(),
+      binding: LoginBinding(),
+    ),
+  ];
 }
